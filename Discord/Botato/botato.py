00bab8693df52faee16e5b717dd3d9e81e02324a
Callama <calebjameshartman@gmail.com>
2018-04-21 22:32:07 -0400

    Added r!restart
    Added r!restart

--- Discord/Botato/botato.py
+++ Discord/Botato/botato.py
@@ -17,9 +17,17 @@ bot.remove_command('help')
 async def on_ready():
     print("DA POTATO IS RIPE AND READY")
     await bot.change_presence(game=discord.Game(name='Pokemon'))
+@bot.command(pass_context=True)
+async def totodile(ctx):
+    await bot.say("Todiles are totolly tubular")
 
 
-
-
+@bot.command(pass_context=True)
+async def restart(ctx):
+    if ctx.message.author.id.startswith('160847568488628225'):
+        await bot.say('The Brotatoes are Restarting the Bot..')
+        os.execv(sys.executable, ['python'] + sys.argv)
+    else:
+        await bot.say("The Brotatoes don't like that you ate there Uncle Fry")
 
 bot.run("NDM3MDcxMzMyNzE5NzIyNTA2.Dbwvpg.VsQ-VvWx88SdYwGehzPgKprDoWY")
